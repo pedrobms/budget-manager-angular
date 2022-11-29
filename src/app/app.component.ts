03799c92ccd3730feeb2c980214cee57dcc11b85
Pedro Bilar Montero <pedro.bilar.montero@gmail.com>
2022-11-29 10:57:12 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'budget-manager-angular';
+  title = 'Budget Manager';
 }
