1c593a8bb3b89629c447cf4c3d245f80d3ef3fbf
Pedro Bilar Montero <pedro.bilar.montero@gmail.com>
2022-12-23 09:10:24 -0300

    Revert "Add all authentication components and implemented login logic"
    This reverts commit f21d2a1d6b8abb80f74025438cde3747f0ad8493.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from './_services/auth.service';
-import { StorageService } from './_services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -8,38 +6,5 @@ import { StorageService } from './_services/storage.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private role: string = '';
-  isLoggedIn = false;
-  showAdminBoard = false;
-  showModeratorBoard = false;
-  username?: string = '';
-
-  constructor(private storageService: StorageService, private authService: AuthService) { }
-
-  ngOnInit(): void {
-    this.isLoggedIn = this.storageService.isLoggedIn();
-
-    if (this.isLoggedIn) {
-      const user = this.storageService.getUser();
-      this.role = user.role;
-      this.showAdminBoard = this.role === 'ROLE_ADMIN';
-      this.showModeratorBoard = this.role === 'ROLE_MODERATOR';
-      this.username = user.name;
-      console.log(user);
-    }
-  }
-
-  logout(): void {
-    this.authService.logout().subscribe({
-      next: data => {
-        console.log(data);
-        this.storageService.signOut();
-
-        window.location.reload();
-      },
-      error: err => {
-        console.log(err);
-      }
-    });
-  }
+  title = 'Budget Manager';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BoardAdminComponent } from './board-admin/board-admin.component';
-import { BoardUserComponent } from './board-user/board-user.component';
-
-import { httpInterceptorProviders } from './_helpers/http.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    ProfileComponent,
-    BoardAdminComponent,
-    BoardUserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule
   ],
-  providers: [httpInterceptorProviders],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
